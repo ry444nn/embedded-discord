8b1c1f1d86c99d74ca7d40b4ccb4b0784a455561
ry444nn <ryan.samaeian+github@proton.me>
2026-02-13 22:09:27 +0330

    updates

--- bot/deploy-commands.js
+++ bot/deploy-commands.js
@@ -1,17 +1,27 @@
-const { SlashCommandBuilder, Routes } = require('discord.js');
-const { REST } = require('@discordjs/rest');
-const { clientId, guildId, token } = require('./config.json');
+const { SlashCommandBuilder, Routes } = require("discord.js");
+const { REST } = require("@discordjs/rest");
+const { clientId, guildId, token } = require("./config.json");
 
 const commands = [
-	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
-	new SlashCommandBuilder().setName('lighton').setDescription('Replies with server info!'),
-    new SlashCommandBuilder().setName('lightoff').setDescription('Replies with server info!'),
-    new SlashCommandBuilder().setName('status').setDescription('Replies with server info!'),
-]
-	.map(command => command.toJSON());
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!"),
+  new SlashCommandBuilder()
+    .setName("lighton")
+    .setDescription("Replies with server info!"),
+  new SlashCommandBuilder()
+    .setName("lightoff")
+    .setDescription("Replies with server info!"),
+  new SlashCommandBuilder()
+    .setName("status")
+    .setDescription("Replies with server info!"),
+].map((command) => command.toJSON());
 
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: "10" }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then((data) => console.log(`Successfully registered ${data.length} application commands.`))
-	.catch(console.error);
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then((data) =>
+    console.log(`Successfully registered ${data.length} application commands.`),
+  )
+  .catch(console.error);
